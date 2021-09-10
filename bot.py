4fc8730d22d19b2b9236bb65080fbc40a8ce1a99
xxxᴛᴇɴᴛᴀᴄɪᴏɴ <81632023+xxxtentacion-TG@users.noreply.github.com>
2021-09-10 12:00:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ bot = Client(
   api_hash="91c026fadfdc442f504a0bd3e5c8cd18",
   )
   
-  @bot.on_message(filters.command(['start']))
+@bot.on_message(filters.command(['start']))
   def start (Client, message):
     message.reply(f"hey bruhh how are you)",
     reply_markup=InlineKeyboardMarkup(
