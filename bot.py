2ed16ddec9d6f54f374e0b7374cf8cb2ec31fbe3
xxxᴛᴇɴᴛᴀᴄɪᴏɴ <81632023+xxxtentacion-TG@users.noreply.github.com>
2021-09-10 12:09:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ bot = Client(
   )
   
 @bot.on_message(filters.command(['Start']))
-  def Start(client, message):
+  def start(client, message):
     message.reply(f"hey bruhh how are you)",
     reply_markup=InlineKeyboardMarkup(
       [
